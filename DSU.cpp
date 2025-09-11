b008a6292672989f34487fb12c5ee2c077309268
Priyanshu <priyanshu@Priyanshus-MacBook-Air.local>
2025-09-11 16:17:52 +0530

    basic understanding

--- DSU.cpp
+++ DSU.cpp
@@ -3,6 +3,49 @@
 using namespace std;    
 using namespace chrono;
 
+/*
+    DSU is a data structure that is used to represent a set of elements.
+    It is used to represent a set of elements that are connected to each other.
+
+
+    1    2   3   4 are 4 different sets currently having 1,2,3,4 as their parent respectively(basically just one element in one set).
+    So we are using DSU to represent these sets.
+    We can represent these sets in two ways:
+    1. By rank
+    2. By size
+
+    By rank:
+    We will keep track of the rank of each set.
+    The rank of a set is the number of elements in the set.
+    The rank of a set is the number of elements in the set. 
+
+    By size:
+    We will keep track of the size of each set.
+    The size of a set is the number of elements in the set.
+    The size of a set is the number of elements in the set. 
+
+    We will use the strategy to decide which way to represent the sets.
+    If the strategy is "rank", we will use the rank to represent the sets.
+    If the strategy is "size", we will use the size to represent the sets.
+
+    We will use the union by rank or union by size to merge the sets.
+    If the strategy is "rank", we will use the rank to merge the sets.
+    If the strategy is "size", we will use the size to merge the sets.
+
+    Union by rank:
+    We will merge the set with the smaller rank into the set with the larger rank.
+    If the ranks are the same, we will merge the set with the smaller rank into the set with the larger rank.
+    If the strategy is "rank", we will use the rank to merge the sets.
+    If the strategy is "size", we will use the size to merge the sets.
+
+    Union by size:
+    We will merge the set with the smaller size into the set with the larger size.
+    If the sizes are the same, we will merge the set with the smaller size into the set with the larger size.
+    If the strategy is "rank", we will use the rank to merge the sets.
+    If the strategy is "size", we will use the size to merge the sets.
+
+    */
+
 class DSU {
     private: 
     vector<int> parent;
